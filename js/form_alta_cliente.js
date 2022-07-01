ff16b8b386963041abaa290282c6f5c77f1c68b8
leoesains <leo.esains@gmail.com>
2022-07-01 10:45:21 -0300

    Se agrega frontend proyecto

---
+++ js/form_alta_cliente.js
@@ -0,0 +1,9 @@
+'use strict';
+    
+    document.querySelector("#btn_volver_alta_cliente").addEventListener("click", volver);
+    
+    function volver(){
+        location.href="cliente.html";
+    }
+
+    
---
+++ js/form_alta_producto.js
@@ -0,0 +1,9 @@
+'use strict';
+    
+    document.querySelector("#btn_volver_alta_producto").addEventListener("click", volver);
+    
+    function volver(){
+        location.href="producto.html";
+    }
+
+    
